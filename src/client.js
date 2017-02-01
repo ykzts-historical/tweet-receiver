5cfee3c3a9ec2cb72bac729bdf2acdc5acb3c69d
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-02-01 10:31:11 +0900

    Unwrap in client script

--- src/client.js
+++ src/client.js
@@ -1,53 +1,60 @@
-(() => {
-  class TweetReceiver {
-    constructor(successCallback = (() => {}), failureCallback = (() => {})) {
-      this.successCallback = successCallback;
-      this.failureCallback = failureCallback;
-      const socket = new WebSocket('ws://localhost:8080/');
-      socket.addEventListener('error', this);
-      socket.addEventListener('message', this);
-      socket.addEventListener('close', this);
-    }
-    handleEvent(event) {
-      const { type } = event;
-      const methodName = `handle${type[0].toUpperCase()}${type.slice(1)}`;
-      return (this[methodName] || (() => {})).call(this, event);
-    }
-    handleError(event) {
-      this.failureCallback(event);
-    }
-    handleMessage({ data }) {
-      const response = JSON.parse(data);
-      const { type } = response;
-      if (type === 'error') {
-        return this.handleError(response);
-      }
-      return this.receiveTweet(response);
-    }
-    handleClose() {}
-    receiveTweet(tweet) {
-      this.successCallback(tweet);
-    }
+class TweetReceiver {
+  constructor(successCallback = (() => {}), failureCallback = (() => {})) {
+    this.successCallback = successCallback;
+    this.failureCallback = failureCallback;
+    const socket = new WebSocket('ws://localhost:8080/');
+    socket.addEventListener('error', this);
+    socket.addEventListener('message', this);
+    socket.addEventListener('close', this);
   }
-  function createTweetItem(tweet) {
-    console.log(tweet);
-    const template = document.getElementById('tweet-template').content;
-    template.querySelector('.user-name').textContent = tweet.user.screen_name;
-    template.querySelector('.user-name').parentNode.setAttribute('href', `https://twitter.com/${tweet.user.screen_name}`);
-    template.querySelector('.text').textContent = tweet.text;
-    return document.importNode(template, true);
+
+  handleEvent(event) {
+    const { type } = event;
+    const methodName = `handle${type[0].toUpperCase()}${type.slice(1)}`;
+    return (this[methodName] || (() => {})).call(this, event);
   }
-  function success(tweet) {
-    const tweetList = document.getElementById('tweets');
-    const tweetItem = createTweetItem(tweet);
-    tweetList.prepend(tweetItem);
+
+  handleError(event) {
+    this.failureCallback(event);
   }
-  function failure() {
-    const element = document.querySelector('[aria-hidden]:not([aria-hidden="false"])');
-    element.setAttribute('aria-hidden', 'false');
+
+  handleMessage({ data }) {
+    const response = JSON.parse(data);
+    const { type } = response;
+    if (type === 'error') {
+      return this.handleError(response);
+    }
+    return this.receiveTweet(response);
   }
-  function main() {
-    new TweetReceiver(success, failure);
+
+  handleClose() {}
+
+  receiveTweet(tweet) {
+    this.successCallback(tweet);
   }
-  main();
-})();
+}
+
+function createTweetItem(tweet) {
+  const template = document.getElementById('tweet-template').content;
+  template.querySelector('.user-name').textContent = tweet.user.screen_name;
+  template.querySelector('.user-name').parentNode.setAttribute('href', `https://twitter.com/${tweet.user.screen_name}`);
+  template.querySelector('.text').textContent = tweet.text;
+  return document.importNode(template, true);
+}
+
+function success(tweet) {
+  const tweetList = document.getElementById('tweets');
+  const tweetItem = createTweetItem(tweet);
+  tweetList.prepend(tweetItem);
+}
+
+function failure() {
+  const element = document.querySelector('[aria-hidden]:not([aria-hidden="false"])');
+  element.setAttribute('aria-hidden', 'false');
+}
+
+function main() {
+  new TweetReceiver(success, failure);
+}
+
+main();
